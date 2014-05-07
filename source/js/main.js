0228ef5b0b11b156c4ea53479afa321dc89cd830
Jermolene <jeremy@osmosoft.com>
2014-05-07 12:51:47 +0100

    Load content from wiki folder

--- source/js/main.js
+++ source/js/main.js
@@ -14,12 +14,19 @@ mainWindow.showDevTools();
 
 var $tw = {};
 
-
+// First part of boot process
 require("../../../../../../../../TiddlyWiki5/boot/bootprefix.js").bootprefix($tw);
 
-$tw.boot = $tw.boot || {};
-$tw.boot.argv = ["*"];
+console.log("working direcory",process.cwd())
 
+// Set command line
+$tw.boot = $tw.boot || {};
+$tw.boot.argv = ["./wiki"];
+
+// Disable rendering
+$tw.boot.disabledStartupModules = ["render"];
+
+// Main part of boot process
 require("../../../../../../../../TiddlyWiki5/boot/boot.js").TiddlyWiki($tw);
 
 // Add some tiddlers
@@ -30,8 +37,11 @@ $tw.wiki.addTiddler({title: "TiddlerTwo", text: "Text of tiddler two"});
 var html = $tw.wiki.renderTiddler("text/html","TiddlerOne",{document: $tw.document});
 console.log(html);
 
+html = $tw.wiki.renderTiddler("text/html","HelloThere",{document: $tw.document});
+console.log(html);
+
 // Require and call a module
-// (We can't use require() because this module is external to TW)
+// (We can't use require() because this module (main.js) is external to TW)
 var u = $tw.modules.execute("$:/core/modules/utils/utils.js");
 console.log(u.htmlEncode("<yes it works!>"));
 
